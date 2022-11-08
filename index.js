099fb1de14d0281a25772f2498211fb70c8e933c
Tuku-Webian <tukuwebian@gmail.com>
2022-11-08 12:03:52 +0600

    MongoDB connected

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fqmp7pn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const Services = client.db("saadDentistry").collection("services");
+  
+
 
 app.get('/', (req, res)=>{
   res.send('SaaD Dentistry server is running...')
