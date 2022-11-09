c46deb43f8eedd3e40d81f5d6fbb881af9a80667
Tuku-Webian <tukuwebian@gmail.com>
2022-11-10 04:45:00 +0600

    Review delete API created

--- index.js
+++ index.js
@@ -81,6 +81,12 @@ async function run() {
       const result = await Reviews.insertOne(review);
       res.send(result);
     })
+    app.delete('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await Reviews.deleteOne(query);
+      res.send(result);
+    })
 
   } finally {
 
